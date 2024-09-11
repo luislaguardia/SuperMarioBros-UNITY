b0ccd625606a1ad0148405e499a812a43411ead0
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-09-11 18:24:24 +0800

    Create SideScrolling.cs

---
+++ SideScrolling.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = player.position.x;
+        transform.position = cameraPosition;
+    }
+
+}
