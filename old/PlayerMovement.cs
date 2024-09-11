75672e1f00246d4bdebac4c82df1ceb94e7fba0a
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-09-11 18:25:02 +0800

    Create PlayerMovement.cs

---
+++ old/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    private new Camera camera;
+    private new Rigidbody2D rigidbody;
+    public float moveSpeed = 8f;
+
+    private Vector2 velocity;
+    private float inputAxis;
+   
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+    }
+
+   private void Update()
+    {
+        HorizontalMovement();
+    }
+
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+
+
+
+        rigidbody.MovePosition(position);
+    }
+
+
+
+}
